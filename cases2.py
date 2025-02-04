cfd6d57520ab2e6e090f87dc69e11ecb55149f7b
yashyp66 <py556530@gmail.com>
2025-02-04 15:51:37 +0530

    Add files via upload

---
+++ cases2.py
@@ -0,0 +1,13 @@
+capital=("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+small=("abcdefghijklmnopqrstuvwxyz")
+
+string=input("enter the small string : ")
+for i in range(len(string)):
+    for j in range(len(capital)):
+        if string[i]==capital[j]:
+            print(small[j],end=" ")
+        elif string[i]==small[j]:
+            print(capital[j],end=" ")
+   
+
+
