296e87b7cb1cdd407bc02f8999fff6ae714deb64
yashyp66 <py556530@gmail.com>
2025-02-18 03:12:56 -0500

    Add files via upload

---
+++ string3.py
@@ -0,0 +1,7 @@
+str1=input("Enter your string 1 :")
+str2=input("Enter your string 2 :")
+if str2 in str1:
+    print(f'"{str2}" is present in "{str1}"')
+else:
+    print(f'"{str2}" is not present in "{str1}"')
+    
